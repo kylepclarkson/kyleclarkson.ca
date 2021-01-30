8a5d796ff30051259638ced2bc773018faa7d6f3
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-01-30 09:21:10 -0800

    Update view

--- mysite/research/views.py
+++ mysite/research/views.py
@@ -17,11 +17,8 @@ class ResearchPostDetail(DetailView):
     model = Article
     template_name = 'research/article_detail.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # context['title'] = str(Article.title)
-        return context
+    def research_post_detail_view(request, pk):
+        post = get_o
 
 
 

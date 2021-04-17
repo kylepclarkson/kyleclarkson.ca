e531542cd5de38ecd8a90f0d94f07ca2ad9c341d
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-04-17 13:06:48 -0700

    Updated UI

--- WebSite/WebSite/settings/development.py
+++ WebSite/WebSite/settings/development.py
@@ -4,14 +4,14 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kyleclarkson_db_1',
-        'USER': 'kyleclarkson_db',
-        'PASSWORD': 'grabgrab566',
-        'HOST': 'database-1.cluo4d9cjysl.us-east-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'kyleclarkson_db_1',
+#         'USER': 'kyleclarkson_db',
+#         'PASSWORD': 'grabgrab566',
+#         'HOST': 'database-1.cluo4d9cjysl.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 

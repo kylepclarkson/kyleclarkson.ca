12fa3728f461518dbef15f63a268b051ec8fe4fe
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-05-13 11:02:19 -0700

    Restarting AWS deployment

--- WebSite/settings/base.py
+++ WebSite/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from . import seckeys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # 2 or 3??
 
 
